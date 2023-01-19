49b5d6f2047cc3f5988c8fa40ddf02cd27cae48b
Darshan1975 <darshangoyani1234@gmail.com>
2023-01-19 19:20:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from 'react'
+import axios from 'axios';
+
+function App() {
+
+  const [data,setdata] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://rickandmortyapi.com/api/character')
+  .then(function (response) {
+    // handle success
+    console.log(response.data.results);
+    setdata(response.data.results)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+  },[]);
+  return (
+    <div className="app">
+      {
+        data.map((item)=>{
+          return(
+            <div className='main'>
+              <div className="photo">
+                <img src={item.image}></img>
+              </div>
+              <div className="caption">
+              <section>
+                <h1>{item.name}</h1>
+                <span>{item.status} - {item.species}</span>
+              </section>
+              <section>
+                <span className='txt-grey'>Last Known location :</span><br></br>
+                <span className='txt'>{item.location.name}</span>
+              </section>
+              <section>
+                <span className='txt-grey'>First seen in :</span><br></br>
+                <span>{item.origin.name}</span>
+              </section>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
